c21364e672224a19171bd0806462384d45773aca
nuggettowin <171437216+nuggettowin@users.noreply.github.com>
2026-09-14 01:00:49 +0800

    First commit

---
+++ part1/autograder/grade.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "This is the script you have to write."
+echo "Remember not to hardcode things!"
+
+# Check if we have enough arguments
+if [[ $# -ge 0 ]]; then
+    exit -1
+fi
+# Delete temporary files
+
+# Compile the reference program
+
+# Generate reference output files
+
+# Remember to check maximum score given as argument, compared to the real number of test cases
+
+# Now mark submissions
+
+#
+# Note: See Lab02Qn.pdf for format of output file. Marks will be deducted for missing elements.
+#
+
+# Iterate over every submission directory
+    # Compile C code
+    # Print compile error message to output file (if any)
+    # Generate output from C code using *.in files in /ref
+    # Compare with reference output files  and award 1 mark if they are identical
+    # print score for student
+# print total submissions marked.
